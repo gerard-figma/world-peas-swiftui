a31f649ba607d315beba640c00f4f015c716a212
Gerard Whote <gerardwhite@icloud.com>
2024-04-23 18:05:12 +0100

    Initial Commit

---
+++ world-peas-swiftui/world_peas_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  world_peas_swiftuiApp.swift
+//  world-peas-swiftui
+//
+//  Created by Gerard White on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct world_peas_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
